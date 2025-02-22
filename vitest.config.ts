420acbbf49a54721238a9d1fd4e6ce5ee406d31b
Michael0520 <michael860520@gmail.com>
2025-02-23 06:12:15 +0800

    feat: add new folder into vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['leetcode/**/*.{test,spec}.{js,ts}'],
+    include: ['leetcode/**/*.{test,spec}.{js,ts}', 'interview/**/*.{test,spec}.{js,ts}'],
     exclude: ['node_modules', '.nuxt', '.output'],
   },
 });
